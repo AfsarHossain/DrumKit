5c12c8b5f172c432b5af9f9e2ec4ee8e1e1ee07b
AfsarHossain <afsar291722@gmail.com>
2021-10-03 01:57:16 +0600

    Script File Added

---
+++ script.js
@@ -0,0 +1,78 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// console.log(numberOfDrumButtons);
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
+
+    // console.log(buttonInnerHTML);
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+/* KEY ACTION */
+document.addEventListener("keypress", function (event) {
+  // console.log(event);
+  // console.log(event.key);
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+
+  //   console.log(activeButton);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
